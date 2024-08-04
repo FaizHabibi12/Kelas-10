dfe68a7ea6c5f8c1e96d1068fb2610d9fd6cbc52
Faiz Habibi <141796990+FaizHabibi12@users.noreply.github.com>
2024-08-04 11:43:00 +0700

    masuk progdas g si harusnya?\

---
+++ Matrix/MalasMenanggapi.java
@@ -0,0 +1,70 @@
+
+// public class MalasMenanggapi {
+//     public static void main(String[] args) {
+        
+//         int[][] matriksA = {{2, 3},{2, 4}};
+//         int[][] matriksB = {{4, 1},{1, 1}};
+
+//         for (int baris = 0; baris < matriksA. length; baris++){
+//             for (int kolom = 0; kolom < matriksB[0].length; kolom++) {
+//                 System.out.print((matriksA[baris][kolom]+matriksB[baris][kolom])+ "\t");
+//             }
+//             System.out.println("");
+//         }
+
+
+//       }
+//     }
+
+
+/**
+ * MalasMenanggapi
+ */
+// public class MalasMenanggapi {
+
+//     public static void main(String[] args) {
+        
+//         int[][] matriksA = {{4,2},{3,1}};
+//         int[][] matriksB = {{1,1},{1,1}};
+
+//             for (int baris = 0; baris < matriksA. length; baris++){
+//                 for (int kolom = 0; kolom < matriksB[0].length; kolom++) {
+//                     System.out.print((matriksA[baris][kolom]+matriksB[baris][kolom])+ "\t");
+//                 }
+//                 System.out.println("");
+//             }
+//     }
+// }
+
+
+public class MalasMenanggapi {
+
+    public static void main(String[] args) {
+        
+        int[][] matriksA = {{7,3},{2,1}};
+        int[][] matriksB = {{4,2},{1,0}};
+
+            for (int baris = 0; baris < matriksA. length; baris++){
+                for (int kolom = 0; kolom < matriksB[0].length; kolom++) {
+                    System.out.print((matriksA[baris][kolom]-matriksB[baris][kolom])+ "\t");
+                }
+                System.out.println("");
+            }
+    }
+}
+
+// public class MalasMenanggapi {
+
+//     public static void main(String[] args) {
+        
+//         int[][] matriksA = {{4,10},{5,2}};
+//         int[][] matriksB = {{7,3},{2,5}};
+
+//             for (int baris = 0; baris < matriksA. length; baris++){
+//                 for (int kolom = 0; kolom < matriksB[0].length; kolom++) {
+//                     System.out.print((matriksA[baris][kolom]-matriksB[baris][kolom])+ "\t");
+//                 }
+//                 System.out.println("");
+//             }
+//     }
+// }
